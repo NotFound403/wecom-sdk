f80ae34d8c447ee6d3e45ab431090ff5a8a54799
Fang <felord@qq.com>
2022-10-19 22:27:23 +0800

    增加一些内置的应用ID

--- src/main/java/cn/felord/DefaultAgent.java
+++ src/main/java/cn/felord/DefaultAgent.java
@@ -1,5 +1,6 @@
 package cn.felord;
 
+import cn.felord.enumeration.NativeAgent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
@@ -19,4 +20,17 @@ public class DefaultAgent implements AgentDetails {
     private final String secret;
     @NonNull
     private final String agentId;
+
+
+    /**
+     * 获取内置基础应用
+     *
+     * @param corpId the corp id
+     * @param secret the secret
+     * @param agent  the agent
+     * @return the agent details
+     */
+    public static AgentDetails nativeAgent(String corpId,String secret,NativeAgent agent){
+        return new DefaultAgent(corpId,secret, agent.getAgentId());
+    }
 }
