f96090ccfc6dab26d2d66e23b9d6e6b92bffc197
xiafang <felord@qq.com>
2022-12-09 17:27:34 +0800

    - 修复回调报错

--- src/main/java/cn/felord/callbacks/XStreamXmlReader.java
+++ src/main/java/cn/felord/callbacks/XStreamXmlReader.java
@@ -9,14 +9,13 @@ import com.thoughtworks.xstream.security.AnyTypePermission;
  * @since 2022/10/10 14:16
  */
 public class XStreamXmlReader implements XmlReader {
-    private final XStream xStream = new XStream(new DomDriver());
-
-
     @SuppressWarnings("unchecked")
     @Override
     public <T> T read(String xml, Class<T> clazz) {
+        XStream xStream = new XStream(new DomDriver());
         xStream.registerConverter(new InstantConverter());
         xStream.addPermission(AnyTypePermission.ANY);
+        xStream.ignoreUnknownElements();
         xStream.processAnnotations(clazz);
         return (T) xStream.fromXML(xml);
     }
