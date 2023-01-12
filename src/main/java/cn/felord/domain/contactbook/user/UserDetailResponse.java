f949fe172ea67446b0e8d23aee4b4ef9587def15
xiafang <felord@qq.com>
2023-01-12 10:04:39 +0800

    修复授权用户信息返回值解析错误的问题

--- src/main/java/cn/felord/domain/contactbook/user/UserDetailResponse.java
+++ src/main/java/cn/felord/domain/contactbook/user/UserDetailResponse.java
@@ -1,6 +1,5 @@
 package cn.felord.domain.contactbook.user;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import cn.felord.domain.WeComResponse;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,12 +7,6 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class UserDetailResponse extends WeComResponse {
-    @JsonAlias("UserId")
-    private String userId;
-    @JsonAlias("DeviceId")
-    private String deviceId;
-    private String userTicket;
-    @JsonAlias("OpenId")
-    private String openId;
-    private String externalUserid;
+    private String userid;
+    private String openid;
 }
