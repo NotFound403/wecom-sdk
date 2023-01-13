2d8604f9e6a0992166a14049c140176be8852feb
xiafang <felord@qq.com>
2023-01-13 18:01:13 +0800

    文本卡片增加按钮自定义字段

--- src/main/java/cn/felord/domain/message/MessageTextCard.java
+++ src/main/java/cn/felord/domain/message/MessageTextCard.java
@@ -1,15 +1,26 @@
 package cn.felord.domain.message;
 
-import lombok.Data;
+import lombok.Getter;
 
 /**
  * @author felord
  * @since 2022/11/22 17:14
  */
-@Data
+@Getter
 public class MessageTextCard {
     private final String title;
     private final String description;
     private final String url;
-    private String btntxt;
+    private final String btntxt;
+
+    public MessageTextCard(String title, String description, String url) {
+        this(title, description, url, null);
+    }
+
+    public MessageTextCard(String title, String description, String url, String btntxt) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.btntxt = btntxt;
+    }
 }
