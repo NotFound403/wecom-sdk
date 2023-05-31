c025e9b3a0b5c527ecd9680597bc5a02a0a9a875
xiafang <felord@qq.com>
2023-05-31 11:39:21 +0800

    - XStream 性能安全优化

--- wecom-sdk/src/main/java/cn/felord/callbacks/XStreamXmlReader.java
+++ wecom-sdk/src/main/java/cn/felord/callbacks/XStreamXmlReader.java
@@ -15,35 +15,46 @@
 
 package cn.felord.callbacks;
 
+import cn.felord.domain.callback.CallbackEventBody;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import com.thoughtworks.xstream.security.AnyTypePermission;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author felord
  * @since 2021/10/10 14:16
  */
 public class XStreamXmlReader implements XmlReader {
-    private static final XStream X_STREAM = init();
+    private static final Map<Class<?>, XStream> XSTREAM_MAP = new HashMap<>();
 
     @SuppressWarnings("unchecked")
     @Override
     public <T> T read(String xml, Class<T> clazz) {
-        X_STREAM.processAnnotations(clazz);
-        return (T) X_STREAM.fromXML(xml);
+        return (T) initXStream(clazz).fromXML(xml);
     }
 
     @Override
     public <T> String write(T t) {
-        X_STREAM.processAnnotations(t.getClass());
-        return X_STREAM.toXML(t);
+        Class<?> clazz = t.getClass();
+        return initXStream(clazz).toXML(t);
     }
 
-    public static XStream init(){
-        XStream xStream = new XStream(new DomDriver());
-        xStream.registerConverter(new InstantConverter());
-        xStream.addPermission(AnyTypePermission.ANY);
-        xStream.ignoreUnknownElements();
+    private static XStream initXStream(Class<?> clazz) {
+        XStream xStream = XSTREAM_MAP.get(clazz);
+        if (xStream == null) {
+            xStream = new XStream(new DomDriver());
+            // 安全白名单
+            Class<?>[] allowTypes = {CallbackXmlBody.class, CallbackEventBody.class};
+            xStream.allowTypes(allowTypes);
+            xStream.registerConverter(new InstantConverter());
+            xStream.addPermission(AnyTypePermission.ANY);
+            xStream.ignoreUnknownElements();
+            xStream.processAnnotations(clazz);
+            XSTREAM_MAP.put(clazz, xStream);
+        }
         return xStream;
     }
 
