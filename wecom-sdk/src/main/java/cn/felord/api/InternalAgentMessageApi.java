e2c98945b578f04d98d68a6c79b430366661c803
Fang <felord@qq.com>
2023-06-29 23:16:19 +0800

    1.1.0

--- wecom-sdk/src/main/java/cn/felord/api/InternalAgentMessageApi.java
+++ wecom-sdk/src/main/java/cn/felord/api/InternalAgentMessageApi.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (c) 2023. felord.cn
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *  Website:
+ *       https://felord.cn
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package cn.felord.api;
 
 import cn.felord.domain.WeComResponse;
@@ -10,7 +25,7 @@ import retrofit2.http.POST;
  * 应用消息
  *
  * @author dax
- * @since 2023/6/29 9:06
+ * @since 2023 /6/29 9:06
  */
 interface InternalAgentMessageApi {
 
@@ -27,6 +42,7 @@ interface InternalAgentMessageApi {
     /**
      * 更新模版卡片消息
      *
+     * @param <R>     the type parameter
      * @param request the request
      * @return the message response
      * @see TemplateReplaceCardBuilders
