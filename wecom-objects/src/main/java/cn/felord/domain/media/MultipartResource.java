eca632431e7659d2090435c09e9ac2d1ca23045b
Fang <felord@qq.com>
2023-06-29 23:08:41 +0800

    - 增加文件扩展名校验 - 增加新的samples样例

--- wecom-objects/src/main/java/cn/felord/domain/media/MultipartResource.java
+++ wecom-objects/src/main/java/cn/felord/domain/media/MultipartResource.java
@@ -1,11 +1,29 @@
+/*
+ *  Copyright (c) 2023. felord.cn
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *  Website:
+ *       https://felord.cn
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package cn.felord.domain.media;
 
+import cn.felord.WeComException;
 import cn.felord.utils.FileTools;
 import lombok.Getter;
 import okhttp3.MediaType;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * The type Multipart resource.
@@ -15,6 +33,7 @@ import java.io.InputStream;
  */
 @Getter
 public class MultipartResource {
+    private static final Pattern pattern = Pattern.compile("\\.([^.]+)$");
     private final MediaType mediaType;
     private final String fileName;
     private final byte[] source;
@@ -55,6 +74,10 @@ public class MultipartResource {
      * @param source    the source
      */
     public MultipartResource(MediaType mediaType, String fileName, byte[] source) {
+        Matcher matcher = pattern.matcher(fileName);
+        if (!matcher.find()) {
+            throw new WeComException("the fileName must have extension, eg. pic.png");
+        }
         this.mediaType = mediaType;
         this.fileName = fileName;
         this.source = source;
